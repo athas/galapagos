8be48ddd3a775bbf554c2beffaa8b1ece5ce811f
dybber <dybber@dybber.dk>
2007-01-12 16:36:35 +0100

    TillTilføjet skabelon til at lave Behaviors ud fra
    darcs-hash:20070112153635-f498b-b77f0bd915b936d6ee4613d9829169bd5721af9c.gz

---
+++ BehaviorSkabelon.java
@@ -0,0 +1,19 @@
+
+public class <navn> implements Behavior
+{
+    /**
+     * 
+     */
+    public Action decide(Finch finch)
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public void response(Action action)
+    {
+        
+    }
+}
