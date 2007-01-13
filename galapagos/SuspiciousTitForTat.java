ab71c9aca77e0dfb2dc60975054b80b12ea2a395
Troels Henriksen <athas@sigkill.dk>
2007-01-13 14:59:55 +0100

    Revised SuspiciousTitForTat.
    darcs-hash:20070113135955-2b36c-e985608e1555947ae69bca9f476c3ec78cdc44ec.gz

--- galapagos/SuspiciousTitForTat.java
+++ galapagos/SuspiciousTitForTat.java
@@ -6,8 +6,15 @@ import java.util.*;
  * A behavior using the suspicious variation of the tit for
  tat-strategy.
  */
-public class SuspiciousTitForTat extends TitForTat {
+public class SuspiciousTitForTat implements Behavior {
   private Map<Finch,Action> finches;
+  
+  /**
+   * Make a new tit for tat-behavior.
+   */
+  public SuspiciousTitForTat () {
+    finches = new HashMap<Finch,Action> ();
+  };
   
   /**
    * Decide what to do to the finch just met. If these finches has met before this finch
@@ -20,11 +27,18 @@ public class SuspiciousTitForTat extends TitForTat {
     } else return Action.IGNORING;
   }
   
+  /**
+   * Remembers what this finch did.
+   */
+  public void response (Finch finch, Action action) {
+    finches.put(finch,action);
+  }
+  
   public Behavior clone() {
       return new SuspiciousTitForTat();
   }
   
   public String toString() {
-      return "TitForTat";
+      return "SuspiciousTitForTat";
   }
 }
