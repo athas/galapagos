fc910b93e71a2f2bf828d0186cb919f254bb1d29
Troels Henriksen <athas@sigkill.dk>
2007-01-13 13:09:33 +0100

    Added BiotopeTest.java file.
    darcs-hash:20070113120933-2b36c-954265e0aad3f24971f45e9421940db3f906f4a5.gz

---
+++ galapagos/tests/BiotopeTest.java
@@ -0,0 +1,13 @@
+package galapagos.tests;
+
+import galapagos.*;
+import junit.framework.*;
+import java.util.*;
+
+public class BiotopeTest extends TestCase {
+
+    public void testBiotope () {
+        Biotope b = new Biotope();
+        b.runRound();
+    }
+}
