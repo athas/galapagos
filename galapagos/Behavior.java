9bf7ed4000929465bd500bcc73c23f5b119f735a
Sune Reeh <Sune@Reeh.dk>
2007-01-14 16:00:09 +0100

    Aendring til Behavior
    darcs-hash:20070114150009-575dc-6889bb9eed19595218163062a607bde5e3ba3278.gz

--- galapagos/Behavior.java
+++ galapagos/Behavior.java
@@ -4,4 +4,9 @@ public interface Behavior extends Cloneable {
   public Action decide (Finch finch);
   public void response (Finch finch, Action action);
   public Behavior clone();
+  
+  /**
+   * The toString()-method must only depend on the class.
+   */
+  public String toString();
 }
