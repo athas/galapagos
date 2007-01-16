0ab7fcbc7d368bad2fd91da8a364cf5cf3fc397e
Sune Reeh <Sune@Reeh.dk>
2007-01-16 15:08:19 +0100

    Javadoc comment in Finch
    darcs-hash:20070116140819-575dc-53ef43bd6157b044b37942e4a987cdfca41b1dc4.gz

--- galapagos/Finch.java
+++ galapagos/Finch.java
@@ -1,5 +1,12 @@
 package galapagos;
 
+/**
+ * The Finch-interface is used for referencing a Finch in places where
+ * the Finch itself is not suposed to be manipulated in any way.
+ */
 public interface Finch {
+    /**
+     * Is this Finch dead or alive?
+     */
     public FinchStatus status ();
 }
