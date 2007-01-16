2056aeb3efa19d5df1175e4affe59091257e2f60
Troels Henriksen <athas@sigkill.dk>
2007-01-16 20:29:41 +0100

    Fixed Finch interface.
    darcs-hash:20070116192941-2b36c-ca455eb06e6911e0ca16ea68c66648cd8578ccc0.gz

--- galapagos/Finch.java
+++ galapagos/Finch.java
@@ -3,10 +3,39 @@ package galapagos;
 /**
  * The Finch-interface is used for referencing a Finch in places where
  * the Finch itself is not suposed to be manipulated in any way.
+ *
+ * <hr>
+ *
+ * Finches are passerine birds, often seed-eating, found chiefly in
+ * the northern hemisphere and Africa. One subfamily is endemic to the
+ * Neotropics. The taxonomic structure of the true finch family,
+ * Fringillidae, is somewhat disputed, with some including the
+ * Hawaiian honeycreepers as another subfamily (Drepanidinae) and/or
+ * uniting the cardueline and fringilline finches as tribes
+ * (Carduelini and Fringillini) in one subfamily; the euphonious
+ * finches were thought to be tanagers due to general similarity in
+ * appearance and mode of life until their real affinities were
+ * realized; the buntings and American sparrows were
+ * formerly considered another subfamily (Emberizinae). Przewalski's
+ * "Rosefinch" (Urocynchramus pylzowi) is now classified as a
+ * distinct, monotypic family with no particularly close
+ * relatives.
+
+ * "Classic" or true finches are small to moderately large and have a
+ * strong, stubby beaks, which in some species can be quite large. All
+ * have tail feathers and 9 primaries. They have a bouncing flight,
+ * alternating bouts of flapping with gliding on closed wings, and
+ * most sing well. Their nests are basket-shaped and built in trees.
+
+ * There are many birds in other families which are often called
+ * finches. These include many species in the very similar-looking
+ * Estrildids or waxbill family, which occur in the Old World tropics
+ * and Australia. Several groups of the Emberizidae family (buntings
+ * and American sparrows) are also named as finches, including the
+ * Darwin's finches of the Galapagos islands, which provided evidence
+ * of Darwin's theory of evolution.
+ *
+ * (Source: Wikipedia)
  */
 public interface Finch {
-    /**
-     * Is this Finch dead or alive?
-     */
-    public FinchStatus status ();
 }
