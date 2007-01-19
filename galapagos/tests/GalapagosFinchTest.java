34f4f50fcafeec29ef28fdd8b5b5fb4dd62530f6
Mikkel Abrahamsen none <none>
2007-01-19 12:34:42 +0100

    Kommentarer og sm ndringer i GalapagosFinchTest
    darcs-hash:20070119113442-527fc-9ba5cf47bd2e62ca5af4e10db5c8873491addfca.gz

--- galapagos/tests/GalapagosFinchTest.java
+++ galapagos/tests/GalapagosFinchTest.java
@@ -3,14 +3,24 @@ package galapagos.tests;
 import junit.framework.TestCase;
 import galapagos.*;
 
+/**
+ * Tests the GalapagosFinch class.
+ */
 public class GalapagosFinchTest extends TestCase {
 
+	/**
+	 * We test the GalapagosFinch class by making a GalapagosFinch and
+	 * checking that the correct values are returned when using each of the
+	 * public quaries, and that we get the right values after using each of the
+	 * public commands.
+	 *
+	 */
 	public void testGalapagosFinch1 () {
 		// Checking new finch.
 		GalapagosFinch fi1 = new GalapagosFinch(5, 10, 2, new Samaritan());
 		assertTrue(fi1.age() == 0);
 		assertTrue(fi1.hitpoints() == 5);
-		assertTrue(fi1.behavior().toString().equals("Samaritan"));
+		assertTrue(fi1.behavior().equals(new Samaritan()));
 		assertTrue(fi1.status() == FinchStatus.ALIVE);
 		
 		// Checking makeOlder and age.
@@ -25,6 +35,7 @@ public class GalapagosFinchTest extends TestCase {
 		fi1.changeHitpoints(0);
 		assertTrue(fi1.hitpoints() == 5);
 		
+		// Checking that the maximal number of hitpoints indeed is 10.
 		fi1.changeHitpoints(12);
 		assertTrue(fi1.hitpoints() == 10);
 		
