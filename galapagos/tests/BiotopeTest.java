4839e0775daff276d92a149d9850f4177c3f85bb
dybber <dybber@dybber.dk>
2007-01-19 01:20:48 +0100

    Refactored some of the tests in BiotopeTest
    darcs-hash:20070119002048-f498b-1be502de45371973c7381e458b6375e8934cfdd2.gz

--- galapagos/tests/BiotopeTest.java
+++ galapagos/tests/BiotopeTest.java
@@ -5,147 +5,141 @@ import junit.framework.*;
 import java.util.*;
 
 public class BiotopeTest extends TestCase {
-
-    public void testStart1 () {
-        // Make a Biotope at size 100x100 with 40 Samaritans,
-    	// Grudgers and FlipFloppers.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
-        // Make a list of the string representations of the behaviors.
-        ArrayList<String> stringList = new ArrayList<String>();
-        for (Behavior be : list) stringList.add(be.toString());
+	ArrayList<Behavior> behaviors;
+	public void setUp() {
+		behaviors = new ArrayList<Behavior>();
+		behaviors.add(new Samaritan());
+		behaviors.add(new Grudger());
+		behaviors.add(new FlipFlopper());
+	}
+	
+	/**
+	 * Tests that the right amount of finches per Behavior is placed in the world.
+	 * Is tested with a 100x100 Biotop and 40 finches per Behavior. 
+	 */
+    public void testConstructor () {       
+        //for counting the number of finches per behavior
+        Map<Behavior, Integer> counter = new HashMap<Behavior, Integer>();
+        //set the count to 0 for all Behaviors.
+        for (Behavior be : behaviors)
+        	counter.put(be, 0);
         
-        Map<String,Integer> m = new WeakHashMap<String,Integer>();
-        for (Behavior be : list)
-        	m.put(be.toString(),0);
-        Biotope b = new
-        	Biotope(100, 100, 1.00, 12, 7, 3, 10, 13, 40, list);
-        World<GalapagosFinch> world = b.world;
-        assertTrue(b.round() == 0);
+        Biotope biotope = new
+        	Biotope(100, 100, 1.00, 12, 7, 3, 10, 13, 40, behaviors);
         
-        // The following tests that the right amount of finches are placed
-        // in the world.
-        // We run through the world and count the number of finches
-        // belonging to each of the behavior types.
-        for (Iterator<World<GalapagosFinch>.Place> it = b.worldIterator();
-        	it.hasNext();)
+        assertEquals(0, biotope.round());
+        
+        // Count the number of finches belonging to each of the Behavior-type.
+        for (World<GalapagosFinch>.Place place : biotope.world)
         {
-        	World<GalapagosFinch>.Place i = it.next();
-        	if (i.getElement() != null)
+        	GalapagosFinch finch = place.getElement();
+        	if (finch != null) //only places with finches should be tested
         	{
-        		GalapagosFinch fi = i.getElement();
-        		// We test that the non-empty place contains a finch with one
-        		// of the behaviors in list, and that the finch has correct
-        		// qualities.
-        		assertTrue(stringList.contains(fi.behavior().toString()));
-        		assertTrue(fi.age() == 1);
-        		assertTrue(fi.status() == FinchStatus.ALIVE);
-        	    m.put(fi.behavior().toString(),m.get(fi.behavior().toString())+1);
+        		// Test that the finch is one of the wanted types
+        		assertTrue(behaviors.contains(finch.behavior()));
+        		
+        		// Test that the finch is alive with age 1.
+        		assertEquals(1, finch.age());
+        		assertEquals(FinchStatus.ALIVE, finch.status());
+
+        		counter.put(finch.behavior(), counter.get(finch.behavior()) + 1);
         	}
         }
         
-        // We test that 40 of each type were counted.
-        for (Behavior be : list)
+        // We test that here are 40 of each Behavior-type.
+        for (Behavior be : behaviors)
         {
-        	assertTrue(m.get(be.toString()) == 40);
-        	Statistics s = b.statistics(be.toString());
-        	assertTrue(s.getBorn() == 0);
-        	assertTrue(s.getBornThisRound() == 0);
-        	assertTrue(s.getDeadByAge() == 0);
-        	assertTrue(s.getDeadByAgeThisRound() == 0);
-        	assertTrue(s.getPopulation() == 40);
+        	assertEquals(40, (int)counter.get(be));
+        	
+        	Statistics stats = biotope.statistics(be);
+        	assertEquals(0, stats.getBorn());
+        	assertEquals(0, stats.getBornThisRound());
+        	assertEquals(0, stats.getDeadByAge());
+        	assertEquals(0, stats.getDeadByAgeThisRound());
+        	assertEquals(40, stats.getPopulation());
         }
     }
     
-    public void testStart2 () {
-        // Make a Biotope at size 4x4 with 0 Samaritans,
-    	// Grudgers and FlipFloppers.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
+    /**
+     * Creates a Biotope with finchesPerBehavior == 0
+     * Tests that the constructor creates an empty World and
+     * the Statistics is zero for all Behaviors.
+     */
+    public void testConstructorNoFinches () {
+        // Make a Biotope at size 4x4 with 0 finches per behavior
         
-        Biotope b = new
-        	Biotope(4, 4, 0.33, 12, 7, 3, 10, 13, 0, list);
-        // The following tests that there are no finches in the world.
-        for (Iterator<World<GalapagosFinch>.Place> it = b.worldIterator();
-        	it.hasNext();)
+        Biotope biotope = new
+        	Biotope(4, 4, 0.33, 12, 7, 3, 10, 13, 0, behaviors);
+        
+        // There should be no finches in the world
+        for (World<GalapagosFinch>.Place place : biotope.world)
+        	assertNull(place.getElement());
+        
+        //Statistics for all behaviors should be 0.
+        for (Behavior be : behaviors)
         {
-        	World<GalapagosFinch>.Place i = it.next();
-        	assertTrue(i.getElement() == null);
-        }
-        for (Behavior be : list)
-        {
-        	Statistics s = b.statistics(be.toString());
-        	assertTrue(s.getBorn() == 0);
-        	assertTrue(s.getBornThisRound() == 0);
-        	assertTrue(s.getDeadByTicks() == 0);
-        	assertTrue(s.getDeadByTicksThisRound() == 0);
-        	assertTrue(s.getDeadByAge() == 0);
-        	assertTrue(s.getDeadByAgeThisRound() == 0);
+        	Statistics stats = biotope.statistics(be);
+        	assertEquals(0, stats.getBorn());
+        	assertEquals(0, stats.getBornThisRound());
+        	assertEquals(0, stats.getDeadByTicks());
+        	assertEquals(0, stats.getDeadByTicksThisRound());
+        	assertEquals(0, stats.getDeadByAge());
+        	assertEquals(0, stats.getDeadByAgeThisRound());
+        	assertEquals(0, stats.getPopulation());
         }
     }
+    
     
     public void testPutFinch () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers. In this biotope the finches don't
-    	// lose any hitpoints, and they become 100 rounds old.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
+    	// loose any hotpoints, and they become 100 rounds old.
         
-        Biotope b = new
-        	Biotope(4, 4, 0.33, 12, 7, 0, 100, 100, 0, list);
-        World<GalapagosFinch> world = b.world;
+        Biotope biotope = new
+        	Biotope(4, 4, 0.33, 12, 7, 0, 100, 100, 0, behaviors);
+        World<GalapagosFinch> world = biotope.world;
     
         // We make a new Samaritan at place (2,2).
-        b.putFinch(2,2, new Samaritan());
+        biotope.putFinch(2,2, new Samaritan());
         
         // Check that the statistics has been correctly updated.
-        Statistics s = b.statistics((new Samaritan()).toString());
-        assertTrue(s.getBorn() == 0);
-    	assertTrue(s.getBornThisRound() == 0);
-    	assertTrue(s.getDeadByTicks() == 0);
-    	assertTrue(s.getDeadByTicksThisRound() == 0);
-    	assertTrue(s.getDeadByAge() == 0);
-    	assertTrue(s.getDeadByAgeThisRound() == 0);
-    	assertTrue(s.getPopulation() == 1);
+        Statistics stats = biotope.statistics((new Samaritan()));
+        assertEquals(0, stats.getBorn());
+        assertEquals(0, stats.getBornThisRound());
+        assertEquals(0, stats.getDeadByTicks());
+        assertEquals(0, stats.getDeadByTicksThisRound());
+        assertEquals(0, stats.getDeadByAge());
+        assertEquals(0, stats.getDeadByAgeThisRound());
+        assertEquals(1, stats.getPopulation());
     	
-    	World<GalapagosFinch>.Place p = world.getAt(2, 2);
-    	assertTrue(p.getElement() != null);
+    	World<GalapagosFinch>.Place place = world.getAt(2, 2);
+    	assertNotNull(place.getElement());
     	
-    	GalapagosFinch fi = p.getElement();
-    	assertTrue(fi.age() == 1);
-		assertTrue(fi.behavior().toString().equals("Samaritan"));
-		assertTrue(fi.status() == FinchStatus.ALIVE);
+    	GalapagosFinch finch = place.getElement();
+    	assertEquals(1, finch.age());
+		assertEquals("Samaritan", finch.behavior().toString());
+		assertEquals(FinchStatus.ALIVE, finch.status());
 		
 		// Make a new samaritan and check that the statistics is still OK.
-		b.putFinch(0,2, new Samaritan());
+		biotope.putFinch(0, 2, new Samaritan());
         
         // Check that the statistics has been correctly updated.
-        assertTrue(s.getBorn() == 0);
-    	assertTrue(s.getBornThisRound() == 0);
-    	assertTrue(s.getDeadByTicks() == 0);
-    	assertTrue(s.getDeadByTicksThisRound() == 0);
-    	assertTrue(s.getDeadByAge() == 0);
-    	assertTrue(s.getDeadByAgeThisRound() == 0);
-    	assertTrue(s.getPopulation() == 2);
+        assertEquals(0, stats.getBorn());
+        assertEquals(0, stats.getBornThisRound());
+        assertEquals(0, stats.getDeadByTicks());
+        assertEquals(0, stats.getDeadByTicksThisRound());
+        assertEquals(0, stats.getDeadByAge());
+        assertEquals(0, stats.getDeadByAgeThisRound());
+        assertEquals(2, stats.getPopulation());
 	}
     
     public void testBreed () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers. In this biotope the finches don't
-    	// lose any hitpoints, and they become 100 rounds old.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
+    	// loose any hotpoints, and they become 100 rounds old.
         
         Biotope b = new
-        	Biotope(4, 4, 0.33, 12, 7, 0, 100, 100, 0, list);
+        	Biotope(4, 4, 0.33, 12, 7, 0, 100, 100, 0, behaviors);
         World<GalapagosFinch> world = b.world;
     
         // We make a new Samaritan at place (2,2).
@@ -155,7 +149,7 @@ public class BiotopeTest extends TestCase {
     	
     	GalapagosFinch fi = p.getElement();
     	
-    	Statistics s = b.statistics(fi.behavior().toString());
+    	Statistics s = b.statistics(fi.behavior());
 		
     	// Repeat making new rounds until the population has increased.
 		// The finch fi should make a new finch at some point before it
@@ -177,35 +171,17 @@ public class BiotopeTest extends TestCase {
     	assertTrue(fi2.behavior().toString() == "Samaritan");
     	assertTrue(fi2.age() == 1);
     	assertTrue(fi2.status() == FinchStatus.ALIVE);
-        
-        // Test that there are only 2 filled Places in the World, and that these are p and newPlace.
-        List<World<GalapagosFinch>.Place> filledPlaces = new LinkedList<World<GalapagosFinch>.Place>();
-        for (Iterator<World<GalapagosFinch>.Place> it = b.worldIterator();
-        it.hasNext();)
-        {
-            World<GalapagosFinch>.Place place = it.next();
-            if (place.getElement() != null)
-                filledPlaces.add(place);
-        }
-        assertTrue(filledPlaces.size() == 2);
-        assertTrue(filledPlaces.contains(p));
-        assertTrue(filledPlaces.contains(newPlace));
     }
     
     public void testMeeting () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers. In this biotope the finches loose
-    	// 2 hitpoint per round and have 1 initial hitpoint.
+    	// 1 hitpoint per round and have 1 initial hitpoint.
     	// Therefore, they die in the first round if they don't
-    	// make any meetings, or participate in a meeting where 
-        // neither finch cleans the other.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
+    	// make any meetings.
         
         Biotope b = new
-        	Biotope(4, 4, 0.00, 10, 1, 2, 100, 100, 0, list);
+        	Biotope(4, 4, 0.00, 10, 1, 1, 100, 100, 0, behaviors);
         World<GalapagosFinch> world = b.world;
     
         // We make a new Samaritan at place (2,2) and one at place (2,3).
@@ -230,14 +206,10 @@ public class BiotopeTest extends TestCase {
     public void testGrimReaperDeadByAge () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers. In this biotope the finches loose
-    	// 0 hitpoint per round, and die at the end of the second round.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
-        
+    	// 0 hitpoint per round, and become 2 rounds old.
+    	
         Biotope b = new
-        	Biotope(4, 4, 0.00, 10, 1, 0, 3, 3, 0, list);
+        	Biotope(4, 4, 0.00, 10, 1, 0, 3, 3, 0, behaviors);
         World<GalapagosFinch> world = b.world;
     
         // We make a new Samaritan at place (2,2).
@@ -256,7 +228,7 @@ public class BiotopeTest extends TestCase {
     	// fi should be in the world:
     	assertTrue(world.getAt(2, 2).getElement() == fi);
     	
-    	Statistics s = b.statistics("Samaritan");
+    	Statistics s = b.statistics(new Samaritan());
     	
     	assertTrue(s.getBorn() == 0);
     	assertTrue(s.getBornThisRound() == 0);
@@ -287,16 +259,11 @@ public class BiotopeTest extends TestCase {
     public void testGrimReaperDeadByTicks () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers. In this biotope the finches loose
-    	// 2 hitpoint per round (and get 1 hitpoint if they don't meet
-        // other finches) and have 2 initial hitpoint.
-    	// Therefore, they are dead after two runRounds.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
-        
+    	// 1 hitpoint per round and have 2 initial hitpoint.
+    	// Therefore, they are dead after two rundRounds
+
         Biotope b = new
-        	Biotope(4, 4, 0.00, 10, 2, 2, 100, 100, 0, list);
+        	Biotope(4, 4, 0.00, 10, 2, 1, 100, 100, 0, behaviors);
         World<GalapagosFinch> world = b.world;
     
         // We make a new Samaritan at place (2,2).
@@ -310,12 +277,12 @@ public class BiotopeTest extends TestCase {
     	assertTrue(fi.age() == 2);
     	
     	// fi should be alive
-    	assertTrue(fi.status() == FinchStatus.ALIVE);
+    	assertEquals(FinchStatus.ALIVE, fi.status());
     	
     	// fi should be in the world
     	assertTrue(world.getAt(2, 2).getElement() == fi);
     	
-    	Statistics s = b.statistics("Samaritan");
+    	Statistics s = b.statistics(new Samaritan());
     	
     	assertTrue(s.getBorn() == 0);
     	assertTrue(s.getBornThisRound() == 0);
@@ -330,7 +297,7 @@ public class BiotopeTest extends TestCase {
     	assertTrue(fi.age() == 3);
     	
     	// fi1 should be dead by ticks
-    	assertTrue(fi.status() == FinchStatus.DEAD_TICKS);
+    	assertEquals(FinchStatus.DEAD_TICKS, fi.status());
     	
     	// fi1 should not be in the world:
     	assertTrue(world.getAt(2, 2).getElement() == null);
@@ -346,13 +313,9 @@ public class BiotopeTest extends TestCase {
     public void testRound () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
-        
+
         Biotope b = new
-        	Biotope(4, 4, 0.00, 10, 2, 1, 100, 100, 0, list);
+        	Biotope(4, 4, 0.00, 10, 2, 1, 100, 100, 0, behaviors);
         
         assertTrue(b.round() == 0);
         b.runRound();
@@ -363,14 +326,10 @@ public class BiotopeTest extends TestCase {
     public void testBehaviors () {
         // Make a Biotope at size 4x4 with 0 Samaritans,
     	// Grudgers and FlipFloppers.
-    	ArrayList<Behavior> list = new ArrayList<Behavior>();
-        list.add(new Samaritan());
-        list.add(new Grudger());
-        list.add(new FlipFlopper());
         
         Biotope b = new
-        	Biotope(4, 4, 0.00, 10, 2, 1, 100, 100, 0, list);
+        	Biotope(4, 4, 0.00, 10, 2, 1, 100, 100, 0, behaviors);
         
-        assertTrue(b.behaviors() == list);
+        assertTrue(b.behaviors() == behaviors);
     }
 }
