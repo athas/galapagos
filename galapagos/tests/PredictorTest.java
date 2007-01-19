ba59f9078376292281509eb587807992907ada37
Troels Henriksen <athas@sigkill.dk>
2007-01-19 12:51:20 +0100

    Prettified and commentificated PredictorTest.
    darcs-hash:20070119115120-2b36c-4c356aeaeb35f07b20f2869a76b14e62f205aa45.gz

--- galapagos/tests/PredictorTest.java
+++ galapagos/tests/PredictorTest.java
@@ -2,16 +2,18 @@ package galapagos.tests;
 
 import galapagos.*;
 
-
+/**
+ * Test that the Predictor uses the two-bit algorithm properly.
+ */
 public class PredictorTest extends BehaviorTest {
     public void testDecide()
     {
         // By default, clean (a Predictor is a nice finch!)
         assertEquals(behavior.decide(opponent), Action.CLEANING);
-        
+
         // Then let it make a cleaning prediction.
         behavior.response(opponent, Action.CLEANING);
-        
+
         // Now a strong clean prediction, which should make the
         // Predictor ignore.
         assertEquals(behavior.decide(opponent), Action.IGNORING);
@@ -21,7 +23,7 @@ public class PredictorTest extends BehaviorTest {
 
         // So clean.
         assertEquals(behavior.decide(opponent), Action.CLEANING);
-        
+
         // Go to strong ignore prediction.
         behavior.response(opponent, Action.IGNORING);
 
@@ -34,11 +36,10 @@ public class PredictorTest extends BehaviorTest {
         // So ignore.
         assertEquals(behavior.decide(opponent), Action.IGNORING);
     }
-    
+
     public Behavior getBehavior() {
         return new Predictor();
     }
-    
 
     public void testToString () {
     	assertEquals(behavior.toString(),"Predictor");
