d1dc51b4596b1e87ca72a74fe4e79392b80f2426
Sune Reeh <Sune@Reeh.dk>
2007-01-20 17:40:52 +0100

    added StatisticalTest
    darcs-hash:20070120164052-575dc-eaaee084fdba38d17822e7c25a9014b447249dbc.gz

---
+++ galapagos/tests/StatisticalTest.java
@@ -0,0 +1,53 @@
+package galapagos.tests;
+
+import galapagos.*;
+
+/**
+ * Test that the Statistical behavior works properly.
+ *
+ * The decision-test only makes sure that no exceptions are
+ * thrown. This is because it uses statistical and probabilistic evaluations
+ * that make it hard to determine how the behavior reacts in specific
+ * situations. 
+ * Also, we do not believe that it makes sense to test the decision-making 
+ * aspect of the behavior, as the determination algorithm is not specified, 
+ * but an implementation detail.
+ */
+public class StatisticalTest extends BehaviorTest {
+    public void testDecide()
+    {
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.CLEANING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.CLEANING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.CLEANING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.IGNORING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.CLEANING);
+        behavior.decide(opponent);
+        behavior.response(opponent, Action.CLEANING);
+        behavior.decide(opponent);
+    }
+
+    public Behavior getBehavior() {
+        return new Statistical();
+    }
+
+    public String behaviorName () {
+    	return "Statistical";
+    }
+}
