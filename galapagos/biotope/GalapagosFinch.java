ed4cb591cc3443385ea288580dea747c26f06d32
Troels Henriksen <athas@sigkill.dk>
2007-01-21 13:53:15 +0100

    Updated GalapagosFinch Javadoc.
    darcs-hash:20070121125315-2b36c-ed334d12e02c8e228b39582cf6aec148df8ead17.gz

--- galapagos/biotope/GalapagosFinch.java
+++ galapagos/biotope/GalapagosFinch.java
@@ -5,78 +5,81 @@ package galapagos.biotope;
  */
 public class GalapagosFinch implements Finch {
   
-  private int hitpoints;
-  private final int maxHitpoints;
-  private int age;
-  private final int maxAge;
+    private int hitpoints;
+    private final int maxHitpoints;
+    private int age;
+    private final int maxAge;
   
-  private final Behavior behavior;
+    private final Behavior behavior;
   
-  /**
-   * Make er new finch with specified hitpoints, maximal age and behavior.
-   */
-  public GalapagosFinch (int hitpoints, int maxHitpoints, int maxAge, Behavior behavior) {
-    this.hitpoints = hitpoints;
-    this.maxHitpoints = maxHitpoints;
-    this.age = 0;
-    this.maxAge = maxAge;
-    this.behavior = behavior;
-  }
+    /**
+     * Make er new finch with specified hitpoints, maximal age and behavior.
+     */
+    public GalapagosFinch (int hitpoints, int maxHitpoints, int maxAge, Behavior behavior) {
+        this.hitpoints = hitpoints;
+        this.maxHitpoints = maxHitpoints;
+        this.age = 0;
+        this.maxAge = maxAge;
+        this.behavior = behavior;
+    }
   
-  /**
-   * What this finch chooses to do to the finch it has met.
-   */
-  public Action decide (Finch finch) {
-    return behavior.decide(finch);
-  }
+    /**
+     * What this finch chooses to do to the finch it has met.
+     */
+    public Action decide (Finch finch) {
+        return behavior.decide(finch);
+    }
   
-  /**
-   * Is this finch dead or alive?
-   */
-  public FinchStatus status () {
-    if (hitpoints <= 0) return FinchStatus.DEAD_TICKS;
-    if (age >= maxAge) return FinchStatus.DEAD_AGE;
-      else return FinchStatus.ALIVE;
-  }
+    /**
+     * Is this finch dead or alive?
+     */
+    public FinchStatus status () {
+        if (hitpoints <= 0) return FinchStatus.DEAD_TICKS;
+        if (age >= maxAge) return FinchStatus.DEAD_AGE;
+        else return FinchStatus.ALIVE;
+    }
   
-  /**
-   * Change this finch's hitpoints by the specified amount.
-   */
-  public void changeHitpoints (int change) {
-    hitpoints += change;
-    if (hitpoints > maxHitpoints)
-        hitpoints = maxHitpoints;
-  }
+    /**
+     * Change this finch's hitpoints by the specified amount.
+     */
+    public void changeHitpoints (int change) {
+        hitpoints += change;
+        if (hitpoints > maxHitpoints)
+            hitpoints = maxHitpoints;
+    }
   
-  /**
-   * Make this finch older.
-   */
-  public void makeOlder () {
-    age++;
-  }
+    /**
+     * Make this finch older.
+     */
+    public void makeOlder () {
+        age++;
+    }
   
-  /**
-   * The age of the GalapagosFinch. 
-   */
-  public int age () {
-      return age;
-  }
+    /**
+     * The age of the GalapagosFinch. 
+     */
+    public int age () {
+        return age;
+    }
+    
+    /**
+     * Return the amount of hit points this finch currently has.
+     */
+    public int hitpoints () {
+        return hitpoints;
+    }
   
-  public int hitpoints () {
-	  return hitpoints;
-  }
+    /**
+     * Get the response of a finch this finch has met.
+     */
+    public void response (Finch finch, Action action) {
+        behavior.response(finch, action);
+    }
   
-  /**
-   * Get the response of a finch this finch has met.
-   */
-  public void response (Finch finch, Action action) {
-    behavior.response(finch, action);
-  }
-  
-  /**
-   * The Behavior of this GalapagosFinch.
-   */
-  public Behavior behavior () {
-      return behavior;
-  }
+    /**
+     * The Behavior of this GalapagosFinch.
+     */
+    public Behavior behavior () {
+        return behavior;
+    }
 } 
